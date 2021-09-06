cccb3a372a292d8d08496336817629d2246b38e3
nanana-na-nashi <nanashous@gmail.com>
2021-09-06 19:43:10 -0300

    Implementação do Computeshading/comparação
    implementação do Computeshading e Comparação de speedup

--- Assets/PhysicManager.cs
+++ Assets/PhysicManager.cs
@@ -41,7 +41,92 @@ public class PhysicManager : MonoBehaviour
 
     void Update()
     {
-        
+        if (startGPU)
+        {
+            time += Time.deltaTime;
+            int totalSize = 3 * sizeof(float) + sizeof(float) + sizeof(float) + sizeof(float) * 4;
+            //passo a variavel data para GPU
+            ComputeBuffer computeBuffer = new ComputeBuffer(data.Length, totalSize);//Aloco um espaço de memoria na GPU
+            computeBuffer.SetData(data);//atribuindo a variavel data na memoria criada
+            computeShader.SetBuffer(1, "balls", computeBuffer);//linkando o buffer ao Kernel de index 1 (Update)
+            computeShader.SetFloat("time", time);//atualiza a variavel time do kernel Update
+            //Executo o CumputeShader
+            computeShader.Dispatch(1, data.Length / 10, 1, 1);
+            lastTime = time;
+
+            //Retorna os dados da GPU para CPU
+            computeBuffer.GetData(data);
+
+            computeBuffer.Dispose();
+
+            int c = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i].velocity == 0)
+                {
+                    c++;
+                }
+                else if (objects[i].transform.position.y <= 0.5f)
+                {
+                    Vector3 pos = objects[i].transform.position;
+                    data[i].pos = new Vector3(pos.x, 0.5f, pos.z);
+                    data[i].velocity = 0;
+                    data[i].color = Random.ColorHSV();
+                }
+                //atualiza o gameobject
+                objects[i].transform.position = data[i].pos;
+                objects[i].GetComponent<MeshRenderer>().material.SetColor("_Color", data[i].color);
+            }
+            if (c == data.Length && endTime == 0)
+            {
+                endTime = time;
+                string txt = "GPU " + count * count + " = " + endTime;
+                Debug.Log(txt);
+            }
+        }
+        else if (startCPU)
+        {
+            time += Time.deltaTime;
+            int c = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i].velocity == 0)
+                {
+                    c++;
+                }
+                else
+                {
+                    //ball.velocity += (acceleration / ball.mass) * delta;
+                    //ball.pos += float3(0, -ball.velocity, 0) * delta;
+                    //ball.color = float4(0, 0, 1, 1);
+
+                    if (objects[i].transform.position.y <= 0.5f)
+                    {
+                        Vector3 pos = objects[i].transform.position;
+                        data[i].pos = new Vector3(pos.x, 0.5f, pos.z);
+                        data[i].velocity = 0;
+                        data[i].color = Random.ColorHSV();
+                    }
+                    else
+                    {
+                        //atualiza os dados
+                        float delta = time - lastTime;
+                        data[i].velocity += (9.8f / data[i].mass) * delta;
+                        data[i].pos += new Vector3(0, -data[i].velocity, 0) * delta;
+                    }
+                }
+                //atualiza o gameobject
+                objects[i].transform.position = data[i].pos;
+                objects[i].GetComponent<MeshRenderer>().material.SetColor("_Color", data[i].color);
+            }
+            lastTime = time;
+            if (c == data.Length && endTime == 0)
+            {
+                endTime = time;
+                string txt = "CPU " + count * count + " = " + endTime;
+                Debug.Log(txt);
+            }
+        }
     }
     //desenha os botÃµes e os campos de texto
     private void OnGUI()
